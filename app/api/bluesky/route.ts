069902909b1fda798df4ad6561271ec7a0d4608a
Astrid Gealer <astrid@gealer.email>
2024-11-19 14:44:19 +0000

    try discord-verify

--- app/api/bluesky/route.ts
+++ app/api/bluesky/route.ts
@@ -1,5 +1,5 @@
 import type { AppBskyFeedPost } from "@atproto/api";
-import { verifyAsync } from "@noble/ed25519";
+import { isValidRequest } from "discord-verify";
 
 // Defines how the shape of the request body should be.
 type RequestBody = {
@@ -19,24 +19,6 @@ async function handlePost(body: RequestBody) {
 // You should set this from the Bluehook response.
 const PUBLIC_KEY = process.env.BLUESKY_PUBLIC_KEY!;
 
-function fromHex(hex: string) {
-    const bytes = new Uint8Array(hex.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16)));
-    return bytes;
-}
-
-const publicKeyBytes = fromHex(PUBLIC_KEY);
-
-// Function to verify Ed25519 signature
-async function verifySignature(message: string, signature: string) {
-    const messageBytes = new TextEncoder().encode(message);
-    const signatureBytes = fromHex(signature);
-    try {
-        return await verifyAsync(signatureBytes, messageBytes, publicKeyBytes);
-    } catch {
-        return false;
-    }
-}
-
 export async function POST(req: Request) {
     // Verify the signature.
     const signature = req.headers.get("X-Signature-Ed25519");
@@ -45,29 +27,16 @@ export async function POST(req: Request) {
         return new Response("Bad Request - no signature or timestamp", { status: 401 });
     }
     console.log("request signature and timestamp", signature, timestamp);
-    const body = await req.text();
-    const message = timestamp + body;
-
-    // Validate the timestamp
-    const currentTime = Math.floor(Date.now() / 1000);
-    const requestTime = parseInt(timestamp, 10);
-    const timeDifference = currentTime - requestTime;
-    const MAX_TIME_DIFF = 300; // 5 minutes
-
-    // Return here if the request is too old.
-    if (timeDifference > MAX_TIME_DIFF) {
-        return new Response("Bad Request - too old", { status: 401 });
-    }
 
     // Verify the signature
-    const isValid = await verifySignature(message, signature);
+    const isValid = await isValidRequest(req, PUBLIC_KEY);
     if (!isValid) {
         console.log("invalid signature!");
         return new Response("Bad Request - invalid signature", { status: 401 });
     }
 
     // Send to our own handler.
-    await handlePost(JSON.parse(body) as RequestBody);
+    await handlePost(await req.json() as RequestBody);
 
     // Return a 204.
     return new Response(null, { status: 204 });
